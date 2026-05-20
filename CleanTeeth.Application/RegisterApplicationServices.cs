a62dc57baee821263b20a50f0cbfe0a88f5c4fb9
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2026-05-20 23:01:00 +0330

    Added RegisterApplicationServices.

---
+++ CleanTeeth.Application/RegisterApplicationServices.cs
@@ -0,0 +1,18 @@
+﻿using CleanTeeth.Application.Features.DentalOffices.Commands.CreateDentailOffice;
+using CleanTeeth.Application.Features.DentalOffices.Queries.GetDentalOfficeDetail;
+using CleanTeeth.Application.Utilities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanTeeth.Application
+{
+    public static class RegisterApplicationServices
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddTransient<IMediator, SimpleMediator>();
+            services.AddScoped<IRequestHandler<CreateDentalOfficeCommand, Guid>, CreateDentalOfficeHandler>();
+            services.AddScoped<IRequestHandler<GetDentalOfficeDetailQuery, DentalOfficeDetailDTO>, GetDentalOfficeDetailQueryHandler>();
+            return services;
+        }
+    }
+}
