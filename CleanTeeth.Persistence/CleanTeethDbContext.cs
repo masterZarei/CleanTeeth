866127cfd5c02ece7a9a88201a155aa2633c4f83
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2026-05-22 22:12:55 +0330

    Configuring persistence layer.

---
+++ CleanTeeth.Persistence/CleanTeethDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CleanTeeth.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanTeeth.Persistence
+{
+    public class CleanTeethDbContext : DbContext
+    {
+        public CleanTeethDbContext(DbContextOptions<CleanTeethDbContext> options) : base(options)
+        {
+        }
+
+        protected CleanTeethDbContext()
+        {
+        }
+        public DbSet<DentalOffice> DentalOffices { get; set; }
+    }
+}
---
+++ CleanTeeth.Persistence/RegisterPersistenceServices.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanTeeth.Persistence
+{
+    public static class RegisterPersistenceServices
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<CleanTeethDbContext>(options =>
+                options.UseSqlServer("name=CleanTeethConnectionString"));
+            return services;
+        }
+    }
+}
