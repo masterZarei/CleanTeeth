e04c6cc1d7edfd9d86b6e571096abc387c1a6efd
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2026-06-02 18:42:35 +0330

    Added Configurations for dentaloffice.

--- CleanTeeth.Persistence/CleanTeethDbContext.cs
+++ CleanTeeth.Persistence/CleanTeethDbContext.cs
@@ -12,6 +12,12 @@ namespace CleanTeeth.Persistence
         protected CleanTeethDbContext()
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CleanTeethDbContext).Assembly);
+        }
         public DbSet<DentalOffice> DentalOffices { get; set; }
+        public DbSet<Patient> Patients { get; set; }
     }
 }
---
+++ CleanTeeth.Persistence/Configurations/DentalOfficeConfig.cs
@@ -0,0 +1,16 @@
+﻿using CleanTeeth.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanTeeth.Persistence.Configurations
+{
+    public class DentalOfficeConfig : IEntityTypeConfiguration<DentalOffice>
+    {
+        public void Configure(EntityTypeBuilder<DentalOffice> builder)
+        {
+            builder.Property(prop => prop.Name)
+                .HasMaxLength(150)
+                .IsRequired();
+        }
+    }
+}
