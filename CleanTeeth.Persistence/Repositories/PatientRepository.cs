10e2d575b90ffa88681e7bf357eadb24a11f00d8
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2026-06-02 18:52:20 +0330

    Added Patient Repository.

---
+++ CleanTeeth.Persistence/Repositories/PatientRepository.cs
@@ -0,0 +1,13 @@
+﻿using CleanTeeth.Application.Contracts.Repositories;
+using CleanTeeth.Domain.Entities;
+
+namespace CleanTeeth.Persistence.Repositories
+{
+    public class PatientRepository : Repository<Patient>, IPatientRepository
+    {
+        public PatientRepository(CleanTeethDbContext context) 
+            : base(context)
+        {
+        }
+    }
+}
