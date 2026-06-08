c1d1ff6747ea9ddc8829a9834952c27a5e0660c5
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2026-06-08 10:27:34 +0330

    Added GetDentistDetail functionality.

--- CleanTeeth.API/Controllers/DentistController.cs
+++ CleanTeeth.API/Controllers/DentistController.cs
@@ -1,7 +1,9 @@
 ﻿using CleanTeeth.API.DTOs.Dentists;
 using CleanTeeth.API.Utilities;
 using CleanTeeth.Application.Features.Dentists.Commands.CreateDentist;
+using CleanTeeth.Application.Features.Dentists.Queries.GetDentistDetail;
 using CleanTeeth.Application.Features.Dentists.Queries.GetDentistList;
+using CleanTeeth.Application.Features.Patients.Queries.GetPatientDetail;
 using CleanTeeth.Application.Features.Patients.Queries.GetPatientsList;
 using CleanTeeth.Application.Utilities;
 using Microsoft.AspNetCore.Mvc;
@@ -24,6 +26,12 @@ namespace CleanTeeth.API.Controllers
             var result = await _mediator.Send(query);
             HttpContext.InsertPaginationsInformationInHeader(result.TotalAmountOfRecords);
             return result.Elements;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DentistDetailDTO>> Get(Guid id)
+        {
+            var query = new GetDentistDetailQuery { Id = id };
+            return await _mediator.Send(query);
         }
         [HttpPost]
         public async Task<IActionResult> CreateDentist([FromBody] CreateDentistDTO createDentistDTO)
