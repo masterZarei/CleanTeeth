9808d1f3054f4683bd65618c1c152ac87327c33e
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2026-06-10 11:27:40 +0330

    Created CreateAppointmentCommand.

---
+++ CleanTeeth.Application/Features/Appointments/Commands/CreateAppointment/CreateAppointmentCommand.cs
@@ -0,0 +1,13 @@
+﻿using CleanTeeth.Application.Utilities;
+
+namespace CleanTeeth.Application.Features.Appointments.Commands.CreateAppointment
+{
+    public class CreateAppointmentCommand : IRequest<Guid>
+    {
+        public Guid PatientId { get; set; }
+        public Guid DentistId { get; set; }
+        public Guid DentalOfficeId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
