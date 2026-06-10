9aa05bddeaf004833773873aeac0fd8d02ccd88b
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2026-06-10 11:29:40 +0330

    Created CreateAppointmentCommandValidator.

---
+++ CleanTeeth.Application/Features/Appointments/Commands/CreateAppointment/CreateAppointmentCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace CleanTeeth.Application.Features.Appointments.Commands.CreateAppointment
+{
+    public class CreateAppointmentCommandValidator : AbstractValidator<CreateAppointmentCommand>
+    {
+        public CreateAppointmentCommandValidator()
+        {
+            RuleFor(x => x.StartDate)
+                .GreaterThan(x => x.EndDate).WithMessage("Start Date must be before end date");
+
+
+        }
+    }
+}
