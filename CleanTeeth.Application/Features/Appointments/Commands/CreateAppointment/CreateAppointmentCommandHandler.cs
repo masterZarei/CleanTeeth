c0e3c742de3abcd1646adda1975d932d65c63490
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2026-06-10 11:54:24 +0330

    Created CreateAppointmentCommandHandler.

---
+++ CleanTeeth.Application/Features/Appointments/Commands/CreateAppointment/CreateAppointmentCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using CleanTeeth.Application.Contracts.Persistence;
+using CleanTeeth.Application.Contracts.Repositories;
+using CleanTeeth.Application.Exceptions;
+using CleanTeeth.Application.Utilities;
+using CleanTeeth.Domain.Entities;
+using CleanTeeth.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanTeeth.Application.Features.Appointments.Commands.CreateAppointment
+{
+    public class CreateAppointmentCommandHandler : IRequestHandler<CreateAppointmentCommand, Guid>
+    {
+        private readonly IAppointmentRepository _appointmentRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateAppointmentCommandHandler(IAppointmentRepository appointmentRepository, IUnitOfWork unitOfWork)
+        {
+            _appointmentRepository = appointmentRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<Guid> Handle(CreateAppointmentCommand request)
+        {
+            var existsOverlap = await _appointmentRepository.OverlapExists(request.DentistId, request.StartDate, request.EndDate);
+            if (existsOverlap)
+            {
+                throw new CustomValidationException("The dentist has an appointment at the selected time");
+            }
+            var timeInterval = new TimeIntervalValueObject(request.StartDate, request.EndDate);
+            var appointment = new Appointment(request.PatientId, request.DentistId, request.DentalOfficeId, timeInterval);
+
+            try
+            {
+                var result = await _appointmentRepository.Add(appointment);
+                await _unitOfWork.Commit();
+                return result.Id;
+            }
+            catch (Exception)
+            {
+                await _unitOfWork.RollBack();
+                throw;
+            }
+            
+        }
+    }
+}
