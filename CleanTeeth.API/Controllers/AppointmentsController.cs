ec7e16eecfeb4f494bddd47e3c7618b55819655b
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2026-06-10 12:07:39 +0330

    CreateAppointment feature has been implemented completely.

---
+++ CleanTeeth.API/Controllers/AppointmentsController.cs
@@ -0,0 +1,32 @@
+﻿using CleanTeeth.API.DTOs.Appointments;
+using CleanTeeth.Application.Features.Appointments.Commands.CreateAppointment;
+using CleanTeeth.Application.Utilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanTeeth.API.Controllers
+{
+    [ApiController]
+    [Route("api/appointments")]
+    public class AppointmentsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AppointmentsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public async Task<IActionResult> Post(CreateAppointmentDTO createAppointmentDTO)
+        {
+            var command = new CreateAppointmentCommand
+            {
+                PatientId = createAppointmentDTO.PatientId,
+                DentistId = createAppointmentDTO.DentistId,
+                DentalOfficeId = createAppointmentDTO.DentalOfficeId,
+                StartDate = createAppointmentDTO.StartDate,
+                EndDate = createAppointmentDTO.EndDate,
+            };
+            var result = await _mediator.Send(command);
+            return Ok();
+        }
+    }
+}
---
+++ CleanTeeth.API/DTOs/Appointments/CreateAppointmentDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanTeeth.API.DTOs.Appointments
+{
+    public class CreateAppointmentDTO
+    {
+        public Guid PatientId { get; set; }
+        public Guid DentistId { get; set; }
+        public Guid DentalOfficeId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
