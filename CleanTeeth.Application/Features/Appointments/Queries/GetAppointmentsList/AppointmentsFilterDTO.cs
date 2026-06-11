8e2ea9484428a485bcd1e8910377427514f5ae9e
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2026-06-11 10:41:51 +0330

    Created AppointmentsFilterDTO.

---
+++ CleanTeeth.Application/Features/Appointments/Queries/GetAppointmentsList/AppointmentsFilterDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanTeeth.Application.Features.Appointments.Queries.GetAppointmentsList
+{
+    public class AppointmentsFilterDTO
+    {
+        public Guid? PatientId { get; set; }
+        public Guid? DentistId { get; set; }
+        public Guid? DentalOfficeId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
